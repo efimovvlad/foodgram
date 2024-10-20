57e83ca22e2b22fbaa7fe6858d12bbf34980979b
efimovvlad <efimovvlad72@gmail.com>
2024-10-21 02:44:57 +0300

    fix users avatar in admin zone

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.html import format_html
 
 from .models import Subscriptions
 
@@ -15,10 +16,22 @@ class UserAdmin(BaseUserAdmin):
         'username',
         'first_name',
         'last_name',
+        'avatar_tag'
     )
     search_fields = ('email', 'username')
     list_display_links = ('email',)
     ordering = ('date_joined',)
+    BaseUserAdmin.fieldsets += (
+        ('Extra Fields', {'fields': ('avatar',)}),
+    )
+
+    def avatar_tag(self, obj):
+        if obj.avatar:
+            return format_html(
+                '<img src="{}" width="50" height="50"/>'.format(obj.avatar.url)
+            )
+
+    avatar_tag.short_description = 'Аватар'
 
 
 @admin.register(Subscriptions)
